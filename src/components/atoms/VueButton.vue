7145ba98d5aff3fbcc058ec9af7c2b8f1f13ed67
gayarabello <gayarabello@gmail.com>
2022-10-10 18:37:25 +0100

    Remove default components and create custom

---
+++ src/components/atoms/VueButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button v-if="options" class="text-3xl font-bold underline"
+    :style="`         
+        border: ${options.depressed ? `2px solid ${options.color}` : 'none'};
+        background-color:  ${options.depressed ? 'white' : options.color};
+        color: ${options.depressed ? options.color : 'white'};
+    `"
+  >
+    {{ options.msg }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+    type: Object,
+    required: true,
+  }, 
+  } 
+};
+</script>
+
+<style>
+button {
+  padding: 1rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+}
+</style>
+
+
+
+
---
+++ src/components/molecules/VueButtonCollection.vue
@@ -0,0 +1,16 @@
+<script setup>
+import VueButton from "@/components/atoms/VueButton.vue";
+
+const props = defineProps({
+    buttons: {
+        type: Array,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div >
+        <VueButton v-for="(button, index) in props.buttons" :key="index" class="mr-8" :options="button"/>  
+    </div>
+</template>
